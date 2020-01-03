38b68ce06d392c2799f89b9157ed345686d84385
Bhavya SIngh <bhavya4official@gmail.com>
2020-01-03 17:01:23 +0530

    Update class_example_inside.java

--- class_example_inside.java
+++ class_example_inside.java
@@ -14,6 +14,10 @@ public static void main(String arg[]){
 }
 
 /*
+0
+Null
+*/
+/*
 A variable which is created inside the class but outside the method is known as an instance variable.
 Instance variable doesn't get memory at compile time.
 It gets memory at runtime when an object or instance is created.
