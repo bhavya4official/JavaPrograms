1556e5adcc7544c2998d5f33c1f60e39acd07e5d
Bhavya SIngh <bhavya4official@gmail.com>
2020-01-03 17:11:02 +0530

    Create class_example_outside.java

---
+++ class_example_outside.java
@@ -0,0 +1,13 @@
+//Creating Student class.  
+class Student{  
+ int id;  
+ String name;  
+}  
+//Creating another class TestStudent1 which contains the main method  
+class TestStudent1{  
+ public static void main(String args[]){  
+  Student s1=new Student();  
+  System.out.println(s1.id);  
+  System.out.println(s1.name);  
+ }  
+}  
